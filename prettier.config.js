19f2318e531e153b2363bc5454264093e1b5be16
aliSohani <a.sohani.shahrood@gmail.com>
2024-11-19 15:33:08 +0330

    brought out the repo from the mono repo

--- prettier.config.js
+++
@@ -1,7 +0,0 @@
-const globalConfig = require("@repo/prettier-config")
-
-/** @type {import('prettier').Config} */
-module.exports = {
-  ...globalConfig,
-  plugins: [...globalConfig.plugins, "prettier-plugin-tailwindcss"],
-}
